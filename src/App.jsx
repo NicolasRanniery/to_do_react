f5d0b10fb1700ed8494b883dc5bdb73b6ef990a1
NicolasRanniery <nicolasranniery123@gmail.com>
2024-09-12 10:31:21 -0300

    Finalização do filtro (ordem alfabética)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import Todo from "./Components/Todo";
 import TodoForm from "./Components/TodoForm";
@@ -28,6 +29,8 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -64,11 +67,23 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
           )
           .map((todo) => (
             <Todo
